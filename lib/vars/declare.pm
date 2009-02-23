597ee98723650ada8f80b427aca846543ba6a89a
kamipo <kamipo@gmail.com>
2009-02-23 22:37:37 +0900

    initial commit

---
+++ lib/vars/declare.pm
@@ -0,0 +1,33 @@
+package vars::declare;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+vars::declare -
+
+=head1 SYNOPSIS
+
+  use vars::declare;
+
+=head1 DESCRIPTION
+
+vars::declare is
+
+=head1 AUTHOR
+
+Ryuta Kamizono E<lt>kamipo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
