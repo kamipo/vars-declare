174543efc52bfd90fdea0ea5b488170bebfe36d1
kamipo <kamipo@gmail.com>
2009-02-23 22:43:56 +0900

    first commit

--- lib/vars/declare.pm
+++ lib/vars/declare.pm
@@ -4,6 +4,29 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Filter::Simple;
+
+FILTER {
+    s/\s(var\s+.*);/ BEGIN {$1}/g;
+};
+
+sub import {
+    my $caller = caller;
+
+    no strict 'refs';
+    *{"$caller\::var"} = \&var;
+}
+
+sub var {
+    my $caller = caller;
+    my ($name, $value) = @_; 
+
+    return unless +($name || '') =~ m{^[a-zA-Z_]\w*$};
+
+    no strict 'refs';
+    *{"$caller\::$name"} = sub () : lvalue {$value};
+}
+
 1;
 __END__
 
